8c94ed45aedc8e23dbaf047b05e3dda4703c0600
luked123 <luked@uoregon.edu>
2018-10-17 15:31:59 -0700

    C# work involving Serialization

---
+++ CSharp18/CSharp18/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CSharp18
+{
+    [Serializable()]
+    public class Animal : ISerializable
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public int AnimalID { get; set; }
+
+        public Animal() { }
+
+        public Animal(string name = "No Name", double wieght = 0, double height = 0)
+        {
+            Name = name;
+            Weight = wieght;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Name} weighs {Weight} lbs and is {Height} inches tall");
+        }
+
+
+        // SerializationInfo is a way to organize data into key value pairs 
+        // in the  use of files
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Weight", Weight);
+            info.AddValue("Height", Height);
+        }
+
+        public Animal(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Weight = (double)info.GetValue("Weight", typeof(double));
+            Height = (double)info.GetValue("Height", typeof(double));
+        }
+    }
+}
--- CSharp18/CSharp18/Program.cs
+++ CSharp18/CSharp18/Program.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿/* Luke Donnelly
+ * C# Pt 18.
+ * Covers Serialization, Serialize, Deserialize, BinaryFormatter, XmlSerializer
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 
 namespace CSharp18
 {
@@ -10,6 +19,74 @@ namespace CSharp18
     {
         static void Main(string[] args)
         {
+            Animal bowser = new Animal("Bowser", 45, 25);
+
+            Stream stream = File.Open("AnimalData.dat", FileMode.Create);
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(stream, bowser);
+            stream.Close();
+
+            bowser = null;
+
+            stream = File.Open("AnimalData.dat", FileMode.Open);
+
+            bf = new BinaryFormatter();
+
+            bowser = (Animal)bf.Deserialize(stream);
+            stream.Close();
+
+            Console.WriteLine(bowser.ToString());
+            bowser.Weight = 50;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Animal));
+
+            using(TextWriter tw = new StreamWriter(@"C:\Users\Luke\CSharpData\bowser.xml"))
+            {
+                serializer.Serialize(tw, bowser);
+            }
+
+            bowser = null;
+
+            XmlSerializer deserializer = new XmlSerializer(typeof(Animal));
+
+            TextReader reader = new StreamReader(@"C:\Users\Luke\CSharpData\bowser.xml");
+            object obj = deserializer.Deserialize(reader);
+            bowser = (Animal)obj;
+            reader.Close();
+
+            Console.WriteLine();
+            Console.WriteLine(bowser);
+
+            List<Animal> theAnimals = new List<Animal>
+            {
+                new Animal("Mario", 60, 30),
+                new Animal("Luigi", 55, 24),
+                new Animal("Peach", 40, 20),
+            };
+
+            using(Stream fs = new FileStream(@"C:\Users\Luke\CSharpData\animals.xml", FileMode.Create))
+            {
+                XmlSerializer serializer2 = new XmlSerializer(typeof(List<Animal>));
+                serializer2.Serialize(fs, theAnimals);
+            }
+
+            theAnimals = null;
+
+            XmlSerializer serializer3 = new XmlSerializer(typeof(List<Animal>));
+            using(FileStream fs2 = File.OpenRead(@"C:\Users\Luke\CSharpData\animals.xml"))
+            {
+                theAnimals = (List<Animal>)serializer3.Deserialize(fs2);
+            }
+
+            foreach(Animal animal in theAnimals)
+            {
+                Console.WriteLine(animal);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
